86c2baef9ebec34f593928878920a045a80a1dc8
Paul Zimmer <paulfzimmer@gmail.com>
2015-12-21 02:44:34 -0800

    Working; Added Readme.md; added eslint rules

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,26 +1,97 @@
+/*
+ * Imports
+ */
+
 import {default as visit} from 'unist-util-visit'
 
-export default (processor, options) => {
-  let preserve = (options || {}).preserve || 1
-  let depth = (options || {}).depth || 0
+/*
+ * Constants
+ */
 
-  let transformer = (ast, file) => {
-    visit(ast, 'heading', (node) => {
-      if (depth < 0) {
-        node.depth += Math.abs(depth)
-        if (node.depth + depth > 4) {
-          return node.depth = 4
+const MINHEAD = 4
+const MAXHEAD = 1
+const OPTIONS = {
+  depth: 0,
+  preserve: 1
+}
+
+/*
+ * Expose
+ */
+
+module.exports = plugin
+
+/*
+ * Options helper to deal with default options
+ */
+
+export function handleOptions(options, callback) {
+  return Object.keys(OPTIONS).reduce((prev, curr) => {
+    prev[curr] = curr in prev ? prev[curr] : OPTIONS[curr]
+    return prev
+  }, options)
+}
+
+/*
+ * Main transformer plugin
+ */
+
+export function plugin(processor, options) {
+  options = handleOptions(options)
+
+  return (ast, file) => {
+    return visit(ast, 'heading', (node) => {
+
+      /*
+       * Reduce heading weight
+       *
+       * Example:
+       *   mdast.use(behead, {depth: -2}).process('# Heading')
+       *
+       * Transforms to '### Heading'
+       */
+
+      if (options.depth < 0) {
+        node.depth += Math.abs(options.depth)
+
+        if (node.depth > MINHEAD) {
+
+          /*
+           * Preserve option prevents heading depth reaching zero
+           * ie if preserve is false then a options.depth value >
+           * 4 will result in heading depth 0
+           *
+           * Example:
+           *   mdast.use(behead, {depth: -5, preserve: false}).process('## Heading')
+           *
+           * Transforms to ' Heading\n'
+           */
+
+          if (options.preserve) {
+            return node.depth = MINHEAD
+          }
+          return node.depth = 0
         }
-        return node.depth += depth
+        return node.depth
       }
+
+      /*
+       * Increase heading weight
+       *
+       * Example:
+       *   mdast.use(behead, {depth: 2}).process('### Heading')
+       *
+       * Transforms to '# Heading'
+       */
+
       else {
-        if (node.depth - depth < 1) {
-          return node.depth = 1
+        node.depth -= options.depth
+
+        if (node.depth < MAXHEAD) {
+          return node.depth = MAXHEAD
         }
-        return node.depth -= depth
+        return node.depth
       }
     })
   }
-
-  return transformer
 }
--- src/tst/index.js
+++ src/tst/index.js
@@ -1,7 +1,58 @@
 import * as fs from 'fs'
+import * as tap from 'tap'
 import * as mdast from 'mdast'
 import {default as behead} from '../lib/index'
 
-console.log(
-  mdast.use(behead, {depth: -2}).process('#### Hello')
-)
+tap.test('Mdast plugin to add or remove heading levels', (plugin) => {
+  plugin.test('Add 1 heading level', (assert) => {
+    let expected = '## Heading\n'
+    let actual = mdast
+      .use(behead, {depth: 1})
+      .process('### Heading')
+
+    assert.equal(actual, expected)
+    assert.end()
+  })
+
+  plugin.test('Add max heading level', (assert) => {
+    let expected = '# Heading\n'
+    let actual = mdast
+      .use(behead, {depth: 5})
+      .process('## Heading')
+
+    assert.equal(actual, expected)
+    assert.end()
+  })
+
+  plugin.test('Remove 1 heading level', (assert) => {
+    let expected = '### Heading\n'
+    let actual = mdast
+      .use(behead, {depth: -1})
+      .process('## Heading')
+
+    assert.equal(actual, expected)
+    assert.end()
+  })
+
+  plugin.test('Remove max heading level', (assert) => {
+    let expected = '#### Heading\n'
+    let actual = mdast
+      .use(behead, {depth: -5})
+      .process('## Heading')
+
+    assert.equal(actual, expected)
+    assert.end()
+  })
+
+  plugin.test('Remove max heading w/o preserve', (assert) => {
+    let expected = ' Heading\n'
+    let actual = mdast
+      .use(behead, {depth: -5, preserve: false})
+      .process('## Heading')
+
+    assert.equal(actual, expected)
+    assert.end()
+  })
+
+  plugin.end()
+})
