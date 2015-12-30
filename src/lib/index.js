d2f36abe9e9421f64c0a18fda3e7dfc835929b69
Paul Zimmer <mrzmmr@gmail.com>
2015-12-29 19:32:47 -0800

    Clean src/lib/index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -32,30 +32,30 @@ export default function plugin(processor, options=OPTIONS) {
     return ast.children = ast.children.map((node) => {
 
       if (node.type && node.type === 'heading') {
-//{{{
+
         /*
          * Reduce heading weight
          *
          * Example:
          *   mdast.use(behead, {weight: -2}).process(markdown)
          */
-//}}}
+
         if (options.weight < 0) {
           node.depth += Math.abs(options.weight)
 
           if (node.depth > MINWEIGHT) {
-//{{{
-          /*
-           * Preserve option prevents heading weight reaching zero
-           * ie if preserve is false then a options.weight value >
-           * 4 will result in heading weight 0
-           *
-           * Example:
-           *   mdast.use(behead, {weight: -5, preserve: false}).process('## Heading')
-           *
-           * Transforms to ' Heading\n'
-           */
-//}}}
+
+            /*
+             * Preserve option prevents heading weight reaching zero
+             * ie if preserve is false then a options.weight value >
+             * 4 will result in heading weight 0
+             *
+             * Example:
+             *   mdast.use(behead, {weight: -5, preserve: false}).process('## Heading')
+             *
+             * Transforms to ' Heading\n'
+             */
+
             if (options.preserve) {
               node.depth = MINWEIGHT
             }
@@ -64,7 +64,7 @@ export default function plugin(processor, options=OPTIONS) {
             }
           }
         }
-//{{{
+
         /*
          * Increase heading weight
          *
@@ -73,7 +73,7 @@ export default function plugin(processor, options=OPTIONS) {
          *
          * Transforms to '# Heading'
          */
-//}}}
+
         else {
           node.depth -= options.weight
 
