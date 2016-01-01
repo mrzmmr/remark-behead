79dae63014af2da698e06f1c8ea5d896340dc40c
Paul Zimmer <mrzmmr@gmail.com>
2015-12-31 18:40:12 -0800

    Add documentation in comments

--- Readme.md
+++ Readme.md
@@ -1,55 +1,55 @@
 # remark-behead
 
-
 ===
-
 
 Behead is a [remark](https://github.com/wooorm/remark) plugin to increase and decrease the weight of headings.  Passing a negative value to the weight option will decrease the heading weight and passing a possative value to the weight option will increase the heading weight.
 
+### ...
 
-## Install
+#### 
 
+###### install
 
 ```bash
 npm i --save remark-behead
 ```
 
+#### 
 
-## Example
-
+###### example
 
 ```js
-var remark = require('remark')
-var behead = require('remark-behead')
-
 remark.use(behead).process('## Heading')
 //=> '## Heading\n'
 
-remark.use(behead, {weight: -2}).process('## Heading')
+remark.use(behead, {weight: 1}).process('### Heading')
+//=> '## Heading\n'
+
+remark.use(behead, {weight: -1}).process('### Heading')
 //=> '#### Heading\n'
 
-remark.use(behead, {weight: 2}).process('## Heading')
+remark.use(behead, {weight: 10}).process('### Heading')
 //=> '# Heading\n'
 
-remark.use(behead, {weight: -8}).process('## Heading')
-//=> '#### Heading\n'
+remark.use(behead, {weight: -10}).process('### Heading')
+//=> '###### Heading\n'
 
-remark.use(behead, {weight: -8, preserve: false}).process('## Heading')
+remark.use(behead, {weight: -10, preserve: false}).process('### Heading')
 //=> ' Heading\n'
 ```
 
+#### 
 
-## Options
-
+###### default options
 
 ```js
-{
-  preserve: [Boolean], // Default = true
-  weight: [Number]     // Default = 0
+const OPTIONS = {
+  preseerve: true,
+  after: '',
+  weight: 0
 }
 ```
 
-
 ### License
 
-[ISC](./License.md) [https://opensource.org/licenses/ISC](https://opensource.org/licenses/ISC)
+[ISC](./License.md) <https://opensource.org/licenses/ISC>
--- src/lib/index.js
+++ src/lib/index.js
@@ -13,7 +13,7 @@ const MINWEIGHT = 6
 const MAXWEIGHT = 1
 
 /*
- * Default options
+ * Option defaults
  */
 
 const OPTIONS = {
@@ -21,10 +21,6 @@ const OPTIONS = {
   after: '',
   weight: 0
 }
-
-/*
- * Main transformer plugin
- */
 
 export default function plugin(processor, options=OPTIONS) {
   options = defop(options, OPTIONS)
@@ -40,6 +36,7 @@ export default function plugin(processor, options=OPTIONS) {
         })
       }
       else {
+
         if (aswitch) {
           return behead(node, options, (error, node) => {
             return node
@@ -55,30 +52,13 @@ export default function plugin(processor, options=OPTIONS) {
 }
 
 export function behead(node, options, callback) {
-  if (node.type && node.type === 'heading') {
 
-    /*
-     * Reduce heading weight
-     *
-     * Example:
-     *   mdast.use(behead, {weight: -2}).process(markdown)
-     */
+  if (node.type && node.type === 'heading') {
 
     if (options.weight < 0) {
       node.depth += Math.abs(options.weight)
 
       if (node.depth > MINWEIGHT) {
-
-        /*
-         * Preserve option prevents heading weight reaching zero
-         * ie if preserve is false then a options.weight value >
-         * 4 will result in heading weight 0
-         *
-         * Example:
-         *   mdast.use(behead, {weight: -5, preserve: false}).process('## Heading')
-         *
-         * Transforms to ' Heading\n'
-         */
 
         if (options.preserve) {
           node.depth = MINWEIGHT
@@ -88,16 +68,6 @@ export function behead(node, options, callback) {
         }
       }
     }
-
-    /*
-     * Increase heading weight
-     *
-     * Example:
-     *   mdast.use(behead, {weight: 2}).process('### Heading')
-     *
-     * Transforms to '# Heading'
-     */
-
     else {
       node.depth -= options.weight
 
@@ -108,4 +78,46 @@ export function behead(node, options, callback) {
   }
   return callback(null, node)
 }
+
+/**
+ * ### install
+ * ```bash
+ * npm i --save remark-behead
+ * ```
+ */
+
+/**
+ * ### example
+ * ```js
+ * remark.use(behead).process('## Heading')
+ * //=> '## Heading\n'
+ *
+ * remark.use(behead, {weight: 1}).process('### Heading')
+ * //=> '## Heading\n'
+ *
+ * remark.use(behead, {weight: -1}).process('### Heading')
+ * //=> '#### Heading\n'
+ *
+ * remark.use(behead, {weight: 10}).process('### Heading')
+ * //=> '# Heading\n'
+ *
+ * remark.use(behead, {weight: -10}).process('### Heading')
+ * //=> '###### Heading\n'
+ *
+ * remark.use(behead, {weight: -10, preserve: false}).process('### Heading')
+ * //=> ' Heading\n'
+ * ```
+ */
+
+/**
+ * ### default options
+ *
+ * ```js
+ * const OPTIONS = {
+ *   preseerve: true,
+ *   after: '',
+ *   weight: 0
+ * }
+ * ```
+ */
 
